705620c654a8bf868c79c6f001e67bc88adda9e7
devgui <guilherme.santosfructuoso@gmail.com>
2022-03-11 21:49:23 -0300

    'mensagem'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import firebase from './firebaseConnection';
+import './index.css'
+import logo from './assets/gullivera.png';
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function App() {
+  const [musica, setMusica] = useState(''); 
+  const [musicas, setMusicas] = useState([]);
+
+
+  useEffect(()=>{
+    async function loadMusicas(){
+      await firebase.firestore().collection('musicas')
+      .onSnapshot((doc)=>{
+        let minhasMusicas = [];
+
+        doc.forEach((item)=>{
+          minhasMusicas.push({
+            id: item.id,
+            musica: item.data().musica
+          })
+        });
+
+        setMusicas(minhasMusicas);
+
+      })
+    }
+
+
+    loadMusicas();
+
+
+  }, []);
+
+  
+
+  async function handleAdd(){
+    await firebase.firestore().collection('musicas')
+    .add({
+      musica: musica,
+      date_time: new Date().toISOString()
+    })
+    .then(()=>{
+      setMusica('');
+      toast.success('Música enviada com sucesso!');
+    })
+    .catch((error)=>{
+      console.log('ERRO: ' + error);
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+            <img src={logo} alt="logo da banda" />
+          </div>
+
+           
+
+          
+            <span className="message">
+              <b>Escolha sua música!</b>
+            </span>
+            <input type="text" maxlenpgth="30" placeholder="digite o nome da banda e da música" value={musica} onChange={(e) => setMusica(e.target.value)}/>
+            <button  onClick={ handleAdd }>Enviar</button>
+            
+            
+
+          <div className='list'> 
+
+           <label>Músicas escolhidas:</label>
+
+           
+          
+          <ul>
+            
+            {musicas.map((musica)=>{
+              return(
+                <li key={musica.id}>
+                  <span> {musica.musica} </span>
+                </li>
+              )
+            })}
+          </ul>
+       
+          </div>
+      </div>
+    
     </div>
   );
 }
+
+
 
 export default App;
