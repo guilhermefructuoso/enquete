5dffd5bc07dce6ccf8c7463be6eb8c6b3e4152de
devgui <guilherme.santosfructuoso@gmail.com>
2022-03-12 17:08:38 -0300

    teste novo commit

--- src/App.js
+++ src/App.js
@@ -1,114 +1,106 @@
-import { useState, useEffect } from 'react';
-import firebase from './firebaseConnection';
+import { useState, useEffect } from 'react'
+import firebase from './firebaseConnection'
 import './index.css'
-import logo from './assets/draxx_2.png';
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import logo from './assets/draxx_2.png'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-  const [musica, setMusica] = useState(''); 
-  const [musicas, setMusicas] = useState([]);
+    const [musica, setMusica] = useState('')
 
+    const [musicas, setMusicas] = useState([])
 
-  useEffect(()=>{
-    async function loadMusicas(){
-      await firebase.firestore().collection('musicas')
-      .onSnapshot((songs)=>{
-        let minhasMusicas = [];
+    useEffect(() => {
+        async function loadMusicas() {
+            await firebase
+                .firestore()
+                .collection('musicas')
+                .onSnapshot((songs) => {
+                    let minhasMusicas = []
 
-        songs.forEach((item)=>{
-          minhasMusicas.push({
-            id: item.id,
-            musica: item.data().musica,
-            dateTime: item.data().date_time
-          })
-        });
+                    songs.forEach((item) => {
+                        minhasMusicas.push({
+                            id: item.id,
+                            musica: item.data().musica,
+                            dateTime: item.data().date_time,
+                        })
+                    })
 
-        minhasMusicas.sort((songA, songB) => {
-          if (songA.dateTime >  songB.dateTime) {
-            return -1;
-          }
-          if (songA.dateTime <  songB.dateTime) {
-            return 1;
-          }
-            return 0;
-        })
+                    minhasMusicas.sort((songA, songB) => {
+                        if (songA.dateTime > songB.dateTime) {
+                            return -1
+                        }
+                        if (songA.dateTime < songB.dateTime) {
+                            return 1
+                        }
+                        return 0
+                    })
 
+                    setMusicas(minhasMusicas)
+                })
+        }
 
-        setMusicas(minhasMusicas);
+        loadMusicas()
+    }, [])
 
-      })
+    async function handleAdd(e) {
+        e.preventDefault()
+
+        if (musica !== '') {
+            await firebase
+                .firestore()
+                .collection('musicas')
+                .add({
+                    musica: musica,
+                    date_time: new Date().toISOString(),
+                })
+                .then(() => {
+                    setMusica('')
+                    toast.success('Música enviada com sucesso!')
+                })
+                .catch((error) => {
+                    console.log('ERRO: ' + error)
+                })
+        } else {
+            toast.error('Preencha para enviar')
+        }
     }
 
+    return (
+        <div className="container-center">
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="logo da banda" />
+                </div>
 
-    loadMusicas();
+                <span className="message">
+                    <b>Escolha sua música!</b>
+                </span>
+                <input
+                    type="text"
+                    maxlenpgth="30"
+                    placeholder="digite o nome da banda e da música"
+                    value={musica}
+                    onChange={(e) => setMusica(e.target.value)}
+                />
+                <button onClick={handleAdd}>Enviar</button>
 
+                <div className="list">
+                    <label>Músicas escolhidas:</label>
 
-  }, []);
-
-  
-
-  async function handleAdd(){
-    await firebase.firestore().collection('musicas')
-    .add({
-      musica: musica,
-      date_time: new Date().toISOString()
-    })
-    .then(()=>{
-      setMusica('');
-      toast.success('Música enviada com sucesso!');
-    })
-    .catch((error)=>{
-      console.log('ERRO: ' + error);
-    })
-  }
-
-
-  return (
-    
-    <div className="container-center">
-      <div className="login">
-        <div className="login-area">
-            <img src={logo} alt="logo da banda" />
-          </div>
-
-           
-
-          
-            <span className="message">
-              <b>Escolha sua música!</b>
-            </span>
-            <input type="text" maxlenpgth="30" placeholder="digite o nome da banda e da música" value={musica} onChange={(e) => setMusica(e.target.value)}/>
-            <button  onClick={ handleAdd }>Enviar</button>
-            
-            
-
-          <div className='list'> 
-
-           <label>Músicas escolhidas:</label>
-
-           
-          
-          <ul>
-            
-            {musicas.map((musica)=>{
-              return(
-                <li key={musica.id}>
-                  <span> {musica.musica} </span>
-                </li>
-              )
-            })}
-          </ul>
-       
-          </div>
-      </div>
-    
-    </div>
-  );
+                    <ul>
+                        {musicas.map((musica) => {
+                            return (
+                                <li key={musica.id}>
+                                    <span> {musica.musica} </span>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-
-
-export default App;
+export default App
